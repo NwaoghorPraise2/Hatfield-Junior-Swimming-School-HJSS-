276e75b007a4310672fa8442b19de2292336cc89
Nwaoghor Praise Chukunweiken <nwaoghorpraise@admins-air.home>
2024-04-02 23:55:40 +0100

    perf: refactor timetable display for optimal performance

--- src/utils/TimeTableHandler.java
+++ src/utils/TimeTableHandler.java
@@ -6,33 +6,60 @@ import models.Lesson;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 public class TimeTableHandler {
     private LessonController lessonController;
+    private LocalDate currentDate;
+    private LocalDate endDate;
 
 
     public TimeTableHandler() {
         this.lessonController = new LessonController();
+        this.currentDate = LocalDate.now();
+        this.endDate = currentDate.plusWeeks(2);
     }
 
     public void displayTimetable() {
-        // Get the current date
-        LocalDate currentDate = LocalDate.now();
+            printTimeTableHeader();
+            List<Lesson> lessons = lessonController.getAllLessons();
+            printTimeTableBody(lessons);
+    }
 
-        System.out.println("Timetable for the week starting from: " + currentDate);
+    public void displayTimetableByGradeLevel(int gradeLevel) {
+        printTimeTableHeader();
+        List<Lesson> lessons = lessonController.getLessonsByGradeLevel(gradeLevel);
+        printTimeTableBody(lessons);
+    }
+
+    public void displayTimetableByCoachName(String coachName) {
+        printTimeTableHeader();
+        List<Lesson> lessons = lessonController.getLessonsByCoach(coachName);
+        printTimeTableBody(lessons);
+    }
+
+    public void displayTimetableByDayOfWeek(DayOfWeek dayOfTheWeek) {
+        printTimeTableHeader();
+        List<Lesson> lessons = lessonController.getLessonsByDay(dayOfTheWeek);
+        printTimeTableBody(lessons);
+    }
+
+    public void printTimeTableHeader(){
+        System.out.println("Timetable for from: " + currentDate + " " + "to" + " "+endDate);
         System.out.println("-----------------------------------------------------------------------------------------------------------------------------------");
         System.out.println("| Date       | Day         | Coach            | Grade Level | Lesson Ref           | Capacity | Status       | Time Slot           |");
         System.out.println("-----------------------------------------------------------------------------------------------------------------------------------");
 
-        // Display timetable for the next 7 days
-            List<Lesson> lessons = lessonController.getLessonsByGradeLevel(5);
+    }
 
-            for (Lesson lesson : lessons) {
+    public void printTimeTableBody(List<Lesson> lessons){
+        for (Lesson lesson : lessons) {
+            if(lesson.getDate().isAfter(currentDate) && lesson.getDate().isBefore(endDate))
                 System.out.printf("| %-10s | %-11s | %-16s | %-11d | %-20s | %-8d | %-12s | %-19s |%n",
-                        lesson.getDate(), lesson.getDayOfTheWeek(), lesson.getCoach(), lesson.getGradeLevel(),
-                        lesson.getLessonRef(), lesson.getCapacity(), lesson.getStatus(), lesson.getTimeSlot());
-            }
+                    lesson.getDate(), lesson.getDayOfTheWeek(), lesson.getCoach(), lesson.getGradeLevel(),
+                    lesson.getLessonRef(), lesson.getCapacity(), lesson.getStatus(), lesson.getTimeSlot());
+        }
         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------");
     }
 }
